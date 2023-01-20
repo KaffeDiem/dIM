27f317b9cd9905c516bf1a51d9e4d32aca79bc98
Kasper Munch <kaspermunchj@gmail.com>
2023-01-20 18:48:24 +0100

    Refactor and add responsibility to UsernameValidator

--- dIM/Helpers/Validators/UsernameValidator.swift
+++ dIM/Helpers/Validators/UsernameValidator.swift
@@ -6,25 +6,81 @@
 //
 
 import Foundation
+import Combine
 
 class UsernameValidator {
+    struct UserInfo {
+        let id: String
+        let name: String
+    }
+    
+    /// Keys related to username used to access UserDefaults
+    enum Keys: String {
+        case username = "settings.username"
+        case userId = "settings.userid"
+        
+        var value: String {
+            return self.rawValue
+        }
+    }
+    
+    /// State describing the current username
     enum State {
-        case valid
+        case valid(userInfo: UserInfo)
         case error(message: String)
         case undetermined
     }
     
-    func validate(username: String) -> State {
-        if username.count < 4 {
-            return .error(message: "Username is too short")
-        } else if username.count > 16 {
-            return .error(message: "Username is too long")
-        } else if username.contains(" ") {
-            return .error(message: "Username must not contain space")
-        } else if username.isEmpty {
-            return .undetermined
-        } else {
-            return .valid
+    // MARK: Public variables
+    @Published var state: State
+    var userInfo: UserInfo? {
+        guard let username, let userId else { return nil }
+        return .init(id: username, name: userId)
+    }
+    
+    // MARK: Private variables
+    private var username: String? {
+        UserDefaults.standard.string(forKey: Keys.username.value)
+    }
+    
+    private var userId: String? {
+        UserDefaults.standard.string(forKey: Keys.userId.value)
+    }
+    
+    init() {
+        self.state = .undetermined
+        if let userInfo {
+            self.state = .valid(userInfo: userInfo)
         }
+    }
+    
+    @discardableResult
+    /// Sets a new username and saves it to UserDefaults
+    /// - Parameter username: Username to save
+    /// - Returns: Username state
+    func set(username: String) -> State {
+        state = .undetermined
+        let state = validate(username: username)
+        if case .valid(let username) = state {
+            UserDefaults.standard.set(username, forKey: Keys.username.value)
+            UserDefaults.standard.set(userId, forKey: Keys.userId.value)
+        }
+        self.state = state
+        return state
+    }
+    
+    /// Validate a given username
+    ///
+    ///- Note: The given username should not include # or an id
+    ///
+    /// - Parameter username: Username without digits
+    /// - Returns: A state describing the validation
+    private func validate(username: String) -> State {
+        guard username.count >= 4 else { return .error(message: "Username is too short") }
+        guard username.count <= 16 else { return .error(message: "Username is too long") }
+        guard !username.contains(" ") else { return .error(message: "Username cannot include spaces")}
+        
+        let id = String(Int.random(in: 1000 ... 9999))
+        return .valid(userInfo: .init(id: id, name: username))
     }
 }
