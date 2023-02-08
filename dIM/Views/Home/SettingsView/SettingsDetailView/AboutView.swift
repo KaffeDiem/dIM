4b1f3c3a2012dcb05a0974da6bcc69270f9b162b
Kasper Munch <kaspermunchj@gmail.com>
2023-02-08 14:41:36 +0100

    Add email button

--- dIM/Views/Home/SettingsView/SettingsDetailView/AboutView.swift
+++ dIM/Views/Home/SettingsView/SettingsDetailView/AboutView.swift
@@ -18,16 +18,16 @@ struct FeatureCell: View {
         HStack(spacing: 24) {
             image
                 .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 32)
+                .scaledToFit()
                 .foregroundColor(.accentColor)
                 .symbolRenderingMode(.hierarchical)
+                .frame(width: 32, alignment: .leading)
             
             VStack(alignment: .leading, spacing: 2) {
                 Text(title)
                     .font(.subheadline)
                     .fontWeight(.semibold)
-                /// Using `.init(:_)` to render Markdown links for iOS 15+
+                // Using `.init(:_)` to render Markdown links for iOS 15+
                 Text(.init(subtitle))
                     .foregroundColor(.secondary)
                     .font(.subheadline)
@@ -39,19 +39,34 @@ struct FeatureCell: View {
 /// The `About` section in the `SettingsView`.
 /// This is the small **dim** icon in the top of the settings as well as the description.
 struct AboutView: View {
+    @State private var emailHelperAlertIsShown = false
+    
     var body: some View {
         ScrollView {
             VStack(spacing: 20) {
-                FeatureCell(image: Image("appiconsvg"), title: "About dIM", subtitle: "dIM is a decentralized chat app based on Bluetooth.")
+                FeatureCell(image: Image("appiconsvg"), title: "About dIM", subtitle: "dIM is an open-source decentralized chat app based on Bluetooth.")
                 FeatureCell(image: Image(systemName: "network"), title: "Peer-to-peer network", subtitle: "When you send a message to someone, it will go through a peer-to-peer Bluetooth network made up of other dIM users.")
                 FeatureCell(image: Image(systemName: "chevron.left.forwardslash.chevron.right"), title: "Open-Source", subtitle: "The source code of dIM is publicly available. This allow developers to verify and improve dIM to be the best and most secure decentralized messenger available. You can [view the Github repository here](https://github.com/KaffeDiem/dIM).")
                 FeatureCell(image: Image(systemName: "lock.circle"), title: "Encrypted and private", subtitle: "Messages are encrypted so that only you and the receiver can read them, protecting you from prying eyes.")
                 FeatureCell(image: Image(systemName: "bubble.left.and.bubble.right"), title: "Feedback is welcome", subtitle: "You can reach out to us by sending us an email or [visiting our website](https://www.dimchat.org).")
+                    .padding(.bottom, 20)
+                
+                Button {
+                    if !EmailHelper.shared.sendEmail(subject: "dIM Support or Feedback", body: "", to: "support@dimchat.org") {
+                        emailHelperAlertIsShown = true
+                    }
+                } label: {
+                    Text("Email")
+                }
             }
-            .padding([.top, .bottom], 50)
-            .padding([.leading, .trailing], 20)
-            .navigationTitle("Decentralized Instant Messenger")
-            .navigationBarTitleDisplayMode(.inline)
+        }
+        .padding(20)
+        .navigationTitle("Decentralized Instant Messenger")
+        .navigationBarTitleDisplayMode(.inline)
+        .alert("No default mail set", isPresented: $emailHelperAlertIsShown) {
+            Button("OK", role: .cancel) { () }
+        } message: {
+            Text("Set a default mailbox to send an email or use your favorite mail provider and contact us at support@dimchat.org")
         }
     }
 }
