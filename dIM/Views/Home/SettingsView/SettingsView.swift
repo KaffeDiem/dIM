e545c077c10cbdc4132aee8d93970eb909949527
Kasper Munch <kaspermunchj@gmail.com>
2023-02-10 14:06:02 +0100

    Delete dead code

--- dIM/Views/Home/SettingsView/SettingsView.swift
+++ dIM/Views/Home/SettingsView/SettingsView.swift
@@ -49,7 +49,7 @@ struct SettingsView: View {
                         ProgressView()
                     } else {
                         TextField("Choose a username...", text: $usernameTextFieldText, onCommit: {
-                            UIApplication.shared.endEditing()
+                            hideKeyboard()
                             
                             switch usernameValidator.validate(username: usernameTextFieldText) {
                             case .valid, .demoMode:
