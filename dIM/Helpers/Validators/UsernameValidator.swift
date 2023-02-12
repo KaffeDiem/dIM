0770f3f4b119b10b9e7a2f3efd777ed1e2309cc9
Kasper Munch <kaspermunchj@gmail.com>
2023-02-12 13:51:30 +0100

    Update naming

--- dIM/Helpers/Validators/UsernameValidator.swift
+++ dIM/Helpers/Validators/UsernameValidator.swift
@@ -117,10 +117,10 @@ extension UsernameValidator {
         // Add a test conversation
         let conversation = ConversationEntity(context: context)
         conversation.author = "SteveJobs#123456"
-        let prkey = CryptoHandler.generatePrivateKey()
-        let pukey = prkey.publicKey
-        let pukeyStr = CryptoHandler.exportPublicKey(pukey)
-        conversation.publicKey = pukeyStr
+        let privateKey = CryptoHandler.generatePrivateKey()
+        let publicKey = privateKey.publicKey
+        let publicKeyText = CryptoHandler.exportPublicKey(publicKey)
+        conversation.publicKey = publicKeyText
         
         // And fill that conversation with a message
         let firstMessage = MessageEntity(context: context)
