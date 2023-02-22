23a423c89f18738d497102ebb13267fed2b33c5a
Kasper Munch <kaspermunchj@gmail.com>
2023-02-22 14:00:18 +0900

    Remove unnecessary code from SceneDelegate

--- dIM/SceneDelegate.swift
+++ dIM/SceneDelegate.swift
@@ -13,19 +13,13 @@ import UserNotifications
 /// Default class generated for iOS apps.
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-    // Get the managed object context from the shared persistent container.
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     var window: UIWindow?
     lazy var appSession = AppSession(context: context)
 
-    // MARK: Handle adding a new contact when scanning their QR code
-    
-    /// openURLContexts handles deep links for the app. They allow users to easily share
-    /// contact information by scanning friends QR codes. This is the callback function
-    /// which is activated when a user clicks on the dIM-link in the camera app.
-    /// - Parameters:
-    ///   - scene: The current scene that we are on. For some reason always load `HomeView`.
-    ///   - URLContexts: The URL contexts formatted as dim://username//publickey
+    /// Handle deep links for scanning contacts from the camera app.
+    ///
+    /// - Note: URL should be formatted as dim://username//publickey
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
         if let url = URLContexts.first?.url{
             let urlStr = url.absoluteString
@@ -33,25 +27,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
     }
     
-    
-    /// Set up the scene and connect it to our `SetupView`.
-    /// - Parameters:
-    ///   - scene: The scene to set up.
-    ///   - session: This current UISession.
-    ///   - connectionOptions: Not in use.
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        
         migrateIfNecessary()
         
-        // Create the SwiftUI view that provides the window contents.
         let contentView = SetupView()
             .environment(\.managedObjectContext, context)
             .environmentObject(appSession)
         
-        // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             window.rootViewController = UIHostingController(rootView: contentView)
@@ -60,44 +42,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
     }
 
-    /// Not used but must be supported according to the protocol.
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    /// Not used but must be supported according to the protocol.
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    /// Not used but must be supported according to the protocol.
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    /// Not used but must be supported according to the protocol.
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    /// Saves the current context to `CoreData` if the app is backgrounded (the user returns to the homescreen.).
+    /// Save data to CoreData when app is closed
     func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-
-        // Save changes in the application's managed object context when the application transitions to the background.
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
     
-    /// If the user had versions prior to 2.* installed they will now be migrated
-    /// to the new use of the UsernameValidator
+    /// Migrate users prior to version 2.*
     private func migrateIfNecessary() {
         // Username used to be stored as 'Username' in UserDefaults with its digits.
         guard let username = UserDefaults.standard.string(forKey: "Username") else {
@@ -114,6 +64,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         UserDefaults.standard.set(components[0], forKey: UserDefaultsKey.username.rawValue)
         UserDefaults.standard.set(components[2], forKey: UserDefaultsKey.userId.rawValue)
+        UserDefaults.standard.removeObject(forKey: "Username")
     }
 }
 
